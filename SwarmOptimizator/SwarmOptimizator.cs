28890a8b6c641ff93b0b4d416a336d7640f219ce
Михаил Товарнов <misha198921rip@gmail.com>
2017-10-05 15:23:06 +0300

    its alive

---
+++ SwarmOptimizator/SwarmOptimizator.cs
@@ -0,0 +1,15 @@
+﻿using Executor;
+using Microsoft.Research.Oslo;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwarmOptimizator {
+    public class Stepper {
+
+    }
+    public class SwarmOptimizator {
+        public ExecutorAbstract<ParConst,Fitness> Executor { get; set; }
+        public BeeColony colony;
+    }
+}
